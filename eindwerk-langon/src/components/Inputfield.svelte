dd3cacbb16f2a431de44eee45d6f63c59cf5bedd
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-05-15 21:48:04 +0200

    component inputfield

---
+++ eindwerk-langon/src/components/Inputfield.svelte
@@ -0,0 +1,16 @@
+<script>
+  let label = "hey,hoi";
+  let name = "RO";
+  let placeholder;
+  let value = "naam";
+  let id = "1";
+</script>
+
+<div class="inputfield">
+  <div class="inputfield_input">
+    <input {label} {name} {placeholder} {value} {id} />
+  </div>
+  <div></div>
+</div>
+
+<style></style>
---
+++ eindwerk-langon/src/routes/login.svelte
@@ -0,0 +1,22 @@
+<script>
+import Inputfield from "../components/Inputfield.svelte";
+</script>
+
+<style lang="scss">
+  @import "../style/pages/Login_page.scss";
+</style>
+
+<div class="login">
+  <div class="login_content">
+    <div class="login_logo">
+      <img src="/images/logo_langon.svg" alt="logo langon" />
+      <p>i18n your projects</p>
+    </div>
+  </div>
+  <div class="login_fields">
+    <h1>Get Started</h1>
+    <Inputfield  placeholder="ik ben hier"/>
+  </div>
+</div>
+
+

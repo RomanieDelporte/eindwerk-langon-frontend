97febfaea0cf08eed7bee42ec6e031235352dd19
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-06-16 00:12:27 +0200

    last changes

--- eindwerk-langon/src/routes/originals.svelte
+++ eindwerk-langon/src/routes/originals.svelte
@@ -1,14 +1,18 @@
 <style lang="scss">
-  @import "../style/pages/TranslationPage.scss";
+@import "../style/pages/TranslationPage.scss";
 </style>
 
 <script>
-  import Title from "../components/Title.svelte";
-  import OriginalTable from "../components/OriginalTable.svelte";
+import Title from "../components/Title.svelte";
+import OriginalTable from "../components/OriginalTable.svelte";
 </script>
 
-<div class="p-pages">
+<div class="p-pages originals">
   <Title text="Originals" />
+  <p>
+    Everything from your website will be published here, if you want to
+    translate it go to translations tab.
+  </p>
   <div class="table">
     <OriginalTable />
   </div>

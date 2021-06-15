43c4b2d82dd9361b609cf4735e2262e4eb88aa4b
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-06-16 00:13:36 +0200

    last changes

--- eindwerk-langon/src/routes/index.svelte
+++ eindwerk-langon/src/routes/index.svelte
@@ -1,44 +1,38 @@
 <style lang="scss">
-  @import "../style/pages/HomePage.scss";
+@import "../style/pages/HomePage.scss";
 </style>
 
 <script>
-  import {
-    goto
-  } from "@sapper/app";
-  import Button from "../components/Button.svelte";
-  import Title from "../components/Title.svelte";
-  import UserCard from "../components/UserCard.svelte";
-  import ImageUpload from "../components/ImageUpload.svelte";
-  import {
-    checkAuth
-  } from "../routes/auth.js";
-  import {
-    onMount
-  } from "svelte";
+import { goto } from "@sapper/app";
+import Button from "../components/Button.svelte";
+import Title from "../components/Title.svelte";
+import UserCard from "../components/UserCard.svelte";
+import ImageUpload from "../components/ImageUpload.svelte";
+import { checkAuth } from "../routes/auth.js";
+import { onMount } from "svelte";
 
-  let isAuth = false;
+let isAuth = false;
 
-  onMount(async () => {
-    try {
-      isAuth = await checkAuth(["Administrator", "Translators"]);
-      console.log(isAuth);
-      const result = await fetch("https://langon.josdeberdt.be/items/users", {
-        headers: {
-          Authorization: "Bearer " + isAuth.tokens.access_token,
-          Accept: "application/json",
-          "Content-type": "application/json",
-        },
-      });
+onMount(async () => {
+  try {
+    isAuth = await checkAuth(["Administrator", "Translators"]);
+    console.log(isAuth);
+    const result = await fetch("https://langon.josdeberdt.be/items/users", {
+      headers: {
+        Authorization: "Bearer " + isAuth.tokens.access_token,
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+    });
 
-      let usersObject = await result.json();
-      users = usersObject;
-    } catch (err) {
-      console.log(err);
-    }
-  });
+    let usersObject = await result.json();
+    users = usersObject;
+  } catch (err) {
+    console.log(err);
+  }
+});
 
-  export let users;
+export let users;
 </script>
 
 <svelte:head>
@@ -54,7 +48,10 @@
           We ensure smooth<br /> cooperation and <br /> professional translation
         </p>
         <div class="home_button">
-          <Button label="About us" isNormal="{true}" on:click="{() => goto('/about')}" />
+          <Button
+            label="About us"
+            isNormal="{true}"
+            on:click="{() => goto('/about')}" />
         </div>
       </div>
       <div class="home_picture">
